70a411321e231013f3cc19ff6ae10ce1924196fe
Andrew Huntington <andrew.huntington@gmail.com>
2021-06-13 15:30:52 +0900

    Change index.html layout to table

--- scripts/index.js
+++ scripts/index.js
@@ -17,16 +17,27 @@ const addBookToLibrary = (...books) => {
   console.log(myLibrary);
 };
 
-const libraryArea = document.querySelector("#library-area");
+const tableBody = document.getElementById("table-body");
+
+let count = 1;
+let read;
 for (const book of myLibrary) {
-  const bookInfo = document.createElement("div");
-  bookInfo.classList.add("box", "notification", "is-info", "is-light");
+  const bookInfo = document.createElement("tr");
+  // bookInfo.classList.add("box", "notification", "is-info", "is-light");
+  if (book.read) {
+    read = "Yes";
+  } else {
+    read = "No";
+  }
 
   bookInfo.innerHTML = `
-    <h2 class="b-title">${book.title}</h2>
-    <h3 class="b-author">${book.author}</h3>
-    <p class="b-pages">${book.pages}</p>
+    <th>${count}</th>
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.pages}</td>
+    <td>${read}</td>
   `;
 
-  libraryArea.appendChild(bookInfo);
+  tableBody.appendChild(bookInfo);
+  count++;
 }
